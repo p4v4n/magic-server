02643db746162bcb0ea7538a616728d76b339aff
p4v4n <pavan.barcelona@gmail.com>
2017-04-20 11:52:00 +0530

    improved code structure/variable names

--- src/magicserver_clojure/server.clj
+++ src/magicserver_clojure/server.clj
@@ -22,7 +22,7 @@
 
 (def test-get-str "GET / HTTP/1.1\r\nHost: localhost:8888\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/56.0.2924.76 Chrome/56.0.2924.76 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\nDNT: 1\r\nAccept-Encoding: gzip, deflate, sdch, br\r\nAccept-Language: en-GB,en-US;q=0.8,en;q=0.6\r\n\r\n")
 (def test-post-str "POST /submit HTTP/1.1\r\nHost: localhost:8888\r\nConnection: keep-alive\r\nContent-Length: 31\r\nCache-Control: max-age=0\r\nOrigin: http://localhost:8888\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/56.0.2924.76 Chrome/56.0.2924.76 Safari/537.36\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\nDNT: 1\r\nReferer: http://localhost:8888/\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-GB,en-US;q=0.8,en;q=0.6\r\n\r\n")
-(def  test-post-str2 "POST /submit HTTP/1.1\r\nHost: localhost:8888\r\nConnection: keep-alive\r\nContent-Length: 31\r\nCache-Control: max-age=0\r\nOrigin: http://localhost:8888\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/57.0.2987.98 Chrome/57.0.2987.98 Safari/537.36\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\nDNT: 1\r\nReferer: http://localhost:8888/\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-GB,en-US;q=0.8,en;q=0.6\r\n\r\nfirstname=Pavan&lastname=Mantha\r\n\r\n")
+(def test-post-str2 "POST /submit HTTP/1.1\r\nHost: localhost:8888\r\nConnection: keep-alive\r\nContent-Length: 31\r\nCache-Control: max-age=0\r\nOrigin: http://localhost:8888\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/57.0.2987.98 Chrome/57.0.2987.98 Safari/537.36\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\nDNT: 1\r\nReferer: http://localhost:8888/\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-GB,en-US;q=0.8,en;q=0.6\r\n\r\nfirstname=Pavan&lastname=Mantha\r\n\r\n")
 
 
 ;;Parsers
@@ -34,20 +34,19 @@
 
 (defn process-request-first-line
 	[input]
-	(zipmap ["method" "path" "protocol"]
-		(split input #"\s")))
+	(zipmap ["method" "path" "protocol"] (split input #"\s")))
 
 
 (defn header-parser [input]
-	(let [header_list (split input #"\r\n")]
+	(let [header-list (split input #"\r\n")]
 		(loop 
-			[result (process-request-first-line (first header_list))
-			re (rest header_list)]
+			[header-map (process-request-first-line (first header-list))
+			re (rest header-list)]
 			(if (empty? re)
-				result
+				 header-map
 				(recur
 					(assoc 
-						result
+						header-map
 						(first (split (first re) #":\s+"))
 						(second (split (first re) #":\s+")))
 					(rest re))))))
@@ -60,10 +59,10 @@
 	(let [keys-needed (filter #(and (not= % "status") (not= % "Content")) (keys response))
 	      response-string (str (response "status") "\r\n")]
 	      (loop [re response-string
-	      	     k keys-needed]
-	      	     (if (empty? k)
+	      	     h-keys keys-needed]
+	      	     (if (empty? h-keys)
                      (str re "\r\n" (response "Content") "\r\n\r\n")
-	      	         (recur (str re (first k) ": " (response (first k)) "\r\n") (rest k))))))
+	      	         (recur (str re (first h-keys) ": " (response (first h-keys)) "\r\n") (rest h-keys))))))
 
 ;;Handlers
 
@@ -100,8 +99,7 @@
 (defn submit [request response]
 	(let [first-name ((request "content") "firstname")
 		  last-name ((request "content") "lastname")]
-		  (send-html-handler request response (format (slurp "./views/submit.html") first-name last-name))
-		  ))
+		  (send-html-handler request response (format (slurp "./views/submit.html") first-name last-name))))
 
 
 (defn form-parser [request]
@@ -119,7 +117,7 @@
 		  			   content-items (split (form-header-dict "Content-Disposition") #"; ")
 		  			   data (into {} (map #(let [[k v] (clojure.string/split % #"=" 2)] {k v}) content-items))
 		  			   new-data (assoc data "body" form-body)]
-		  			 (recur (rest c-l) (assoc form (data "name") new-data)))))))
+		  			(recur (rest c-l) (assoc form (data "name") new-data)))))))
 
 
 (defn parse-fields [body]
@@ -135,8 +133,8 @@
 
 (defn get-handler [request response]
 	(try
-	(((ROUTES "get") (request "path")) request response)
-	(catch Exception e (err-404-handler request response))))
+	    (((ROUTES "get") (request "path")) request response)
+	    (catch Exception e (err-404-handler request response))))
 
 
 (defn post-handler [request response]
@@ -176,8 +174,8 @@
 (defn check-for-content-length [input]
 	(let [header-dict (header-parser (trim input))
 		  content-length (header-dict "Content-Length")]
-	(if content-length
-        (Integer/parseInt content-length))))
+	    (if content-length
+            (Integer/parseInt content-length))))
 
 
 (defn receive [socket]
@@ -190,19 +188,17 @@
 		  content-length (check-for-content-length request-header)
 		  k (prn "The length of the content is " content-length)
 		  complete-request (if content-length 
-		  	        (let [body (char-array content-length)]
-		  	        	(.read x body 0 content-length)
-		  	        	(str request-header (apply str body)))
-		  	        request-header)
-		  ]
-		 complete-request	  	        
-		 ))
+		  	                   (let [body (char-array content-length)]
+		  	        	           (.read x body 0 content-length)
+		  	        	           (str request-header (apply str body)))
+		  	                   request-header)]
+		 complete-request))
 
 
 (defn send [socket msg]
-  (let [writer (io/writer socket)]
-    (.write writer msg)
-    (.flush writer)))
+    (let [writer (io/writer socket)]
+        (.write writer msg)
+        (.flush writer)))
 
 
 (defn serve-persistent [port handler]
