bde02dc6a9ad5d01c4b3669556d47007d5fce57f
p4v4n <pavan.barcelona@gmail.com>
2017-04-20 12:50:01 +0530

    converted tabs into spaces

--- src/magicserver_clojure/sessions.clj
+++ src/magicserver_clojure/sessions.clj
@@ -8,27 +8,27 @@
 
 
 (defn session-handler [request response]
-	(let [browser-cookies (request "Cookie")]
-		(if (and browser-cookies (browser-cookies "sid") (SESSIONS (browser-cookies "sid")))
-			response
-			(let [cookie (uuid)]
-			    (swap! SESSIONS assoc cookie {})
-				(assoc response "Set-Cookie" (str "sid=" cookie))))))
+    (let [browser-cookies (request "Cookie")]
+        (if (and browser-cookies (browser-cookies "sid") (SESSIONS (browser-cookies "sid")))
+            response
+            (let [cookie (uuid)]
+                (swap! SESSIONS assoc cookie {})
+                (assoc response "Set-Cookie" (str "sid=" cookie))))))
 
 
 (defn add-session [request content]
-	(let [browser-cookies (request "Cookie")]
-		(if (browser-cookies "sid")
-			(swap! SESSIONS assoc (browser-cookies "sid") content))))
+    (let [browser-cookies (request "Cookie")]
+        (if (browser-cookies "sid")
+            (swap! SESSIONS assoc (browser-cookies "sid") content))))
 
 
 (defn get-session [request]
-	(let [browser-cookies (request "Cookie")]
-		(if (and browser-cookies (browser-cookies "sid"))
-			(SESSIONS (browser-cookies "sid")))))
+    (let [browser-cookies (request "Cookie")]
+        (if (and browser-cookies (browser-cookies "sid"))
+            (SESSIONS (browser-cookies "sid")))))
 
 
 (defn del-session [request]
-	(let [browser-cookies (request "Cookie")]
-		(if (browser-cookies "sid")
-			(swap! SESSIONS dissoc (browser-cookies "sid")))))
+    (let [browser-cookies (request "Cookie")]
+        (if (browser-cookies "sid")
+            (swap! SESSIONS dissoc (browser-cookies "sid")))))
