f2eb99fc8e3c362ddad91bf45c280b725421125e
p4v4n <pavan.barcelona@gmail.com>
2017-04-27 13:24:39 +0530

    implemented page re-routing

--- src/main.clj
+++ src/main.clj
@@ -11,20 +11,30 @@
     (let [session-cookie (get-session request)
     	userid (if session-cookie (session-cookie "userid"))]
     (if userid
-    	(send-html-handler request response (format (slurp "./views/dashboard.html") userid))
+    	(redirect-303-handler "/dashboard")
         (send-html-handler request response (slurp "./views/index.html")))))
+
+
+(defn dashboard [request response]
+    (let [session-cookie (get-session request)
+        userid (if session-cookie (session-cookie "userid"))]
+    (if userid
+        (send-html-handler request response (format (slurp "./views/dashboard.html") userid))
+        (redirect-303-handler "/"))))
 
 
 (defn login [request response]
     (let [username ((request "Content") "username")
           password ((request "Content") "password")]
          (add-session request {"userid" username})
-        (send-html-handler request response (format (slurp "./views/dashboard.html") username))))
+         (redirect-303-handler "/dashboard")))
+
 
 (defn logout [request response]
 	(del-session request)
-    (send-html-handler request response (slurp "./views/index.html")))
+    (redirect-303-handler "/"))
 
 (add-route "get" "/" home)
+(add-route "get" "/dashboard" dashboard)
 (add-route "post" "/login" login)
 (add-route "post" "/logout" logout)
